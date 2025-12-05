78fb5069222abc7251da60e957531242729ed4ac
Kai-Li-Galxe <128114707+Kai-Li-Galxe@users.noreply.github.com>
2025-12-05 16:50:35 +0800

    feat: test github action

--- test/playwright/wallet-setup/basic.setup.ts
+++ test/playwright/wallet-setup/basic.setup.ts
@@ -9,4 +9,13 @@ export default defineWalletSetup(PASSWORD, async (context, walletPage) => {
   const metamask = new MetaMask(context, walletPage, PASSWORD)
 
   await metamask.importWallet(SEED_PHRASE)
+
+  await metamask.importWalletFromPrivateKey('0xbf54083c4ac85ea4720d19a0b9d1b3cc3230e77b9693424a653fb81d96c86932')
+
+  console.log('importWalletFromPrivateKey success2222')
+
+  walletPage.screenshot({ 
+    path: 'test-results/wallet-page222.png',
+    fullPage: true 
+  })
 })
