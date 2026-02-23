f915a8599644ff5230b96bcbc90f76eb5560b357
Kai-Li-Galxe <128114707+Kai-Li-Galxe@users.noreply.github.com>
2026-02-23 16:41:56 +0800

    feat: login test

--- test/playwright/util.ts
+++ test/playwright/util.ts
@@ -1,28 +1,43 @@
-import { MetaMask } from '../../src/playwright'
-import basicSetup from './wallet-setup/basic.setup'
 import type { BrowserContext, Page } from '@playwright/test';
+import { MetaMask } from '../../src/playwright';
+import basicSetup from './wallet-setup/basic.setup';
 
-const delay = (ms:number) => new Promise(res => setTimeout(res, ms));
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
-export const handleLogin = async ( url: string, context: BrowserContext, page: Page, extensionId: string)=>{
-  const metamask = new MetaMask(context, page, basicSetup.walletPassword, extensionId)
+export const handleLogin = async (
+  url: string,
+  context: BrowserContext,
+  page: Page,
+  extensionId: string
+) => {
+  const metamask = new MetaMask(
+    context,
+    page,
+    basicSetup.walletPassword,
+    extensionId
+  );
 
-  await page.goto(url);// test
+  await page.goto(url); // test
 
-  await page.locator('.e2e-login-btn').first().click()
-  await delay(3000)
+  await page.locator('.e2e-login-btn').first().click();
+  await delay(3000);
 
-  await page.locator('.e2e-MetaMask').click()
-  console.log('click metamask btn success')
-  await delay(3000)
+  await page.locator('.e2e-MetaMask').click();
+  console.log('click metamask btn success');
+  await delay(3000);
 
-  await metamask.connectToDapp()
+  await metamask.connectToDapp();
 
   // 弹窗之间有延迟
-  await delay(3000)
+  await delay(3000);
 
-  await metamask.confirmSignature()
-  console.log('confirmSignature success')
-  await delay(3000)
-  return page
-}
+  await metamask.confirmSignature();
+  console.log('confirmSignature success');
+  await delay(3000);
+
+  await page.screenshot({
+    path: 'test-results/mm-login222.png',
+    fullPage: true,
+  });
+  return page;
+};
